bb10121dcc8fd9be59c23f049c98ceaa95dbb8d2
zhaixiuchuan <zhaixiuchuan@hotmail.com>
2020-11-17 21:46:37 +0800

    reorganize sdk demo

---
+++ mm_sdk/matrixMul.cu
@@ -0,0 +1,261 @@
+/*
+ * Copyright 1993-2009 NVIDIA Corporation.  All rights reserved.
+ *
+ * NVIDIA Corporation and its licensors retain all intellectual property and 
+ * proprietary rights in and to this software and related documentation and 
+ * any modifications thereto.  Any use, reproduction, disclosure, or distribution 
+ * of this software and related documentation without an express license 
+ * agreement from NVIDIA Corporation is strictly prohibited.
+ * 
+ */
+
+/* Matrix multiplication: C = A * B.
+ * Host code.
+ *
+ * This sample implements matrix multiplication and is exactly the same as
+ * Chapter 7 of the programming guide.
+ * It has been written for clarity of exposition to illustrate various CUDA
+ * programming principles, not with the goal of providing the most
+ * performant generic kernel for matrix multiplication.
+ *
+ * CUBLAS provides high-performance matrix multiplication.
+ */
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// includes, project
+//#include <cutil_inline.h>
+//#include <helper_functions.h>
+
+// includes, kernels
+#include <cuda_runtime_api.h>
+#include "matrixMul.h"
+#include "matrixMul_kernel.cuh"
+
+////////////////////////////////////////////////////////////////////////////////
+// declaration, forward
+void runTest(int argc, char** argv);
+void randomInit(float*, int);
+
+////////////////////////////////////////////////////////////////////////////////
+// Helper Functions
+////////////////////////////////////////////////////////////////////////////////
+#ifndef STRNCASECMP
+#define STRNCASECMP strncasecmp
+#endif
+
+inline int stringRemoveDelimiter(char delimiter, const char *string) {
+    int string_start = 0;
+
+    while (string[string_start] == delimiter) {
+        string_start++;
+    }
+
+    if (string_start >= (int)strlen(string)-1) {
+        return 0;
+    }
+
+    return string_start;
+}
+
+inline bool checkCmdLineFlag(const int argc, const char **argv, const char *string_ref)
+{
+    bool bFound = false;
+
+    if (argc >= 1)
+    {
+        for (int i=1; i < argc; i++)
+        {
+            int string_start = stringRemoveDelimiter('-', argv[i]);
+            const char *string_argv = &argv[i][string_start];
+
+            const char *equal_pos = strchr(string_argv, '=');
+            int argv_length = (int)(equal_pos == 0 ? strlen(string_argv) : equal_pos - string_argv);
+
+            int length = (int)strlen(string_ref);
+
+            if (length == argv_length && !STRNCASECMP(string_argv, string_ref, length))
+            {
+                bFound = true;
+                continue;
+            }
+        }
+    }
+
+    return bFound;
+}
+
+inline int getCmdLineArgumentInt(const int argc, const char **argv, const char *string_ref) {
+    bool bFound = false;
+    int value = -1;
+
+    if (argc >= 1) {
+        for (int i=1; i < argc; i++) {
+            int string_start = stringRemoveDelimiter('-', argv[i]);
+            const char *string_argv = &argv[i][string_start];
+            int length = (int)strlen(string_ref);
+
+            if (!STRNCASECMP(string_argv, string_ref, length)) {
+                if (length+1 <= (int)strlen(string_argv)) {
+                    int auto_inc = (string_argv[length] == '=') ? 1 : 0;
+                    value = atoi(&string_argv[length + auto_inc]);
+                } else {
+                    value = 0;
+                }
+
+                bFound = true;
+                continue;
+            }
+        }
+    }
+
+    if (bFound) {
+        return value;
+    } else {
+        return 0;
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char** argv) {
+    runTest(argc, argv);
+
+    exit(EXIT_SUCCESS);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Run a simple test for CUDA
+////////////////////////////////////////////////////////////////////////////////
+void runTest(int argc, char** argv) {
+
+    /****************************************************/
+    /*  Preparations                                    */
+    /****************************************************/
+
+    printf("[Matrix Multiply Using CUDA] - Starting...\n");
+
+    if (checkCmdLineFlag(argc, (const char **)argv, "help") ||
+        checkCmdLineFlag(argc, (const char **)argv, "?")) {
+        printf("Usage -device=n (n >= 0 for deviceID)\n");
+        printf("      -wA=WidthA -hA=HeightA (Width x Height of Matrix A)\n");
+        printf("      -wB=WidthB -hB=HeightB (Width x Height of Matrix B)\n");
+        printf("  Note: Outer matrix dimensions of A & B matrices must be equal.\n");
+
+        exit(EXIT_SUCCESS);
+    }
+
+    // By default, we use device 0, otherwise we override the device ID based on what is provided at the command line
+    int devID = 0;
+
+    if (checkCmdLineFlag(argc, (const char **)argv, "device")) {
+        devID = getCmdLineArgumentInt(argc, (const char **)argv, "device");
+        cudaSetDevice(devID);
+    }
+
+    cudaError_t error;
+    cudaDeviceProp deviceProp;
+    error = cudaGetDevice(&devID);
+
+    if (error != cudaSuccess) {
+        printf("cudaGetDevice returned error code %d, line(%d)\n", error, __LINE__);
+    }
+
+    error = cudaGetDeviceProperties(&deviceProp, devID);
+
+    if (deviceProp.computeMode == cudaComputeModeProhibited) {
+        fprintf(stderr, "Error: device is running in <Compute Mode Prohibited>, no threads can use ::cudaSetDevice().\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    if (error != cudaSuccess) {
+        printf("cudaGetDeviceProperties returned error code %d, line(%d)\n", error, __LINE__);
+    } else {
+        printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+    }
+
+    // utilities
+    cudaEvent_t start;
+    cudaEvent_t stop;
+    float msecTotal;
+
+    // set seed for rand()
+    srand(2006);
+
+    // allocate host memory for matrices A and B
+    unsigned int size_A = WA * HA;
+    unsigned int mem_size_A = sizeof(float) * size_A;
+    float* h_A = (float*) malloc(mem_size_A);
+    unsigned int size_B = WB * HB;
+    unsigned int mem_size_B = sizeof(float) * size_B;
+    float* h_B = (float*) malloc(mem_size_B);
+    float flop = 2 * (float)WC * (float)HC * (float)WA;
+
+    // initialize host memory
+    randomInit(h_A, size_A);
+    randomInit(h_B, size_B);
+    
+    // allocate device memory
+    float* d_A;
+    cudaMalloc((void**) &d_A, mem_size_A);
+    float* d_B;
+    cudaMalloc((void**) &d_B, mem_size_B);
+
+    // allocate device memory for result
+    unsigned int size_C = WC * HC;
+    unsigned int mem_size_C = sizeof(float) * size_C;
+    float* d_C;
+    cudaMalloc((void**) &d_C, mem_size_C);
+
+    // allocate host memory for the result
+    float* h_C = (float*) malloc(mem_size_C);
+
+    /****************************************************/
+    /*  Tiling without global mem coalescing            */
+    /****************************************************/
+
+    // create and start timer
+    cudaEventCreate(&start);
+    cudaEventRecord(start, NULL);
+    // setup execution parameters
+    auto threads = dim3(BLOCK_SIZE, BLOCK_SIZE);
+    auto grid = dim3(WC / threads.x, HC / threads.y);
+    // copy host memory to device
+    cudaMemcpy(d_A, h_A, mem_size_A, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, mem_size_B, cudaMemcpyHostToDevice);
+    // naive implementation
+    matrixMul<<< grid, threads >>>(d_C, d_A, d_B, WA, WB);
+    // copy result from device to host
+    cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+    // stop and destroy timer
+    cudaEventCreate(&stop);
+    cudaEventRecord(stop, NULL);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&msecTotal, start, stop);
+    printf("Tiling GPU\n");
+    printf("Processing time: %f (ms), GFLOPS: %f \n", msecTotal, flop / msecTotal/ 1e+6);
+
+    /****************************************************/
+    /*  Cleaning                                        */
+    /****************************************************/
+
+    // clean up memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+}
+
+// Allocates a matrix with random float entries.
+void randomInit(float* data, int size) {
+    for (int i = 0; i < size; ++i)
+        data[i] = rand() / (float)RAND_MAX;
+}
